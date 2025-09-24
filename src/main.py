6739c16d0c619631da493c6da697d509b709c632
ayubidrissi <elidrissi.ayub@uni.eus>
2025-09-24 10:41:46 +0200

    h

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from mimetypes import init
+import os
+
+class kalkulagailua:
+
+    def batu(self, a: init , b: init) -> int:
+        return a + b
---
+++ src/test.py
@@ -0,0 +1,4 @@
+from main import kalkulagailua
+
+def test_batu_2_zenbaki():
+    assert kalkulagailua().batu(3, 2) == 5
