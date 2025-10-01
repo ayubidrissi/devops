d05c29500921e3c7d11aa53d84dff3a53c5783e8
ayubidrissi <elidrissi.ayub@uni.eus>
2025-10-01 09:41:08 +0200

    docker-proba

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
-from mimetypes import init
-import os
-
-class kalkulagailua:
-
-    def batu(self, a: init , b: init) -> int:
-        return a + b
+def main():
+ print("=" * 50)
+ print("GitHub Actions + Docker martxan da!")
+ print("=" * 50)
+if __name__ == "__main__":
+ main()
