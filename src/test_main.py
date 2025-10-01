35418a1a1aa0ce0449cd80f329d39fe972cfe378
ayubidrissi <elidrissi.ayub@uni.eus>
2025-10-01 09:51:32 +0200

    aldaketak

--- src/test_main.py
+++
@@ -1,7 +0,0 @@
-from main import kalkulagailua
-
-def test_batu():
-    kalku = kalkulagailua()
-    assert kalku.batu(3, 2) == 5
-    assert kalku.batu(-1, 1) == 0
-    assert kalku.batu(0, 0) == 0
